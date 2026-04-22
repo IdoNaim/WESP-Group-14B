46a0fc0aa27980e28d4e7fabd1509e76017db90c
tomer <tomer0033265@gmail.com>
2026-04-22 16:20:55 +0300

    add System get all UsersInfo with out Auth

--- src/main/java/com/ticketpurchasingsystem/project/domain/systemAdmin/AdminPublisher.java
+++ src/main/java/com/ticketpurchasingsystem/project/domain/systemAdmin/AdminPublisher.java
@@ -1,11 +1,22 @@
 package com.ticketpurchasingsystem.project.domain.systemAdmin;
 
+import java.util.List;
+
 import org.springframework.context.ApplicationEventPublisher;
+
+import com.ticketpurchasingsystem.project.domain.User.UserInfo;
+import com.ticketpurchasingsystem.project.domain.systemAdmin.SystemAdminEvents.GetAllUsersEvent;
 
 class AdminPublisher {
     private final ApplicationEventPublisher eventPublisher;
 
     public AdminPublisher(ApplicationEventPublisher eventPublisher) {
         this.eventPublisher = eventPublisher;
+    }
+
+    public List<UserInfo> publishGetAllUsers(String reqId) {
+        GetAllUsersEvent event = new GetAllUsersEvent(this);
+        eventPublisher.publishEvent(event);
+        return event.getResult(reqId);
     }
 }
