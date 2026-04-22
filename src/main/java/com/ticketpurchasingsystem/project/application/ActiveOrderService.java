d01d717601c33ec5f4c6f08901498b8aa4f02682
IdoNaim <idonai@post.bgu.ac.il>
2026-04-22 09:48:49 +0300

    added saveOrder to interface and service

--- src/main/java/com/ticketpurchasingsystem/project/application/ActiveOrderService.java
+++ src/main/java/com/ticketpurchasingsystem/project/application/ActiveOrderService.java
@@ -1,6 +1,46 @@
 package com.ticketpurchasingsystem.project.application;
-
+import com.ticketpurchasingsystem.project.domain.ActiveOrders.*;
 public class ActiveOrderService implements IActiveOrderService {
     
+    @Override
+    public String createActiveOrder(String userId, String eventId, int quantity) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void cancelActiveOrder(String orderId) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void getActiveOrders(String userId) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void getActiveOrder(String orderId) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void completeActiveOrder(String orderId) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void updateActiveOrder(String orderId, int quantity) {
+        // TODO Auto-generated method stub
+        
+    }
+    @Override
+    public boolean saveOrder(ActiveOrderItem order) {
+        // TODO Auto-generated method stub
+        return false;
+    }
     
 }
--- src/main/java/com/ticketpurchasingsystem/project/application/IActiveOrderService.java
+++ src/main/java/com/ticketpurchasingsystem/project/application/IActiveOrderService.java
@@ -1,5 +1,5 @@
 package com.ticketpurchasingsystem.project.application;
-
+import com.ticketpurchasingsystem.project.domain.ActiveOrders.*;
 public interface  IActiveOrderService {
     public String createActiveOrder(String userId, String eventId, int quantity) ;
     public void cancelActiveOrder(String orderId) ;
@@ -7,6 +7,7 @@ public interface  IActiveOrderService {
     public void getActiveOrder(String orderId) ;
     public void completeActiveOrder(String orderId) ; // maybe add payment info as parameter?
     public void updateActiveOrder(String orderId, int quantity) ;
+    public boolean saveOrder(ActiveOrderItem order);
 
     
 }
