a2e72e47e38841561eb899ede496b3302368ed23
IdoNaim <idonai@post.bgu.ac.il>
2026-04-26 16:55:40 +0300

    commented this function, may not need it

--- src/main/java/com/ticketpurchasingsystem/project/application/ActiveOrderService.java
+++ src/main/java/com/ticketpurchasingsystem/project/application/ActiveOrderService.java
@@ -9,10 +9,12 @@ public class ActiveOrderService implements IActiveOrderService {
     ActiveOrderListener activeOrderListener;
     ActiveOrderPublisher activeOrderPublisher;
     IActiveOrderRepo activeOrderRepo;
-    public ActiveOrderService(ActiveOrderListener activeOrderListener, ActiveOrderPublisher activeOrderPublisher, IActiveOrderRepo activeOrderRepo) {
+    AuthenticationService authenticationService;
+    public ActiveOrderService(ActiveOrderListener activeOrderListener, ActiveOrderPublisher activeOrderPublisher, IActiveOrderRepo activeOrderRepo, AuthenticationService authenticationService) {
         this.activeOrderListener = activeOrderListener;
         this.activeOrderPublisher = activeOrderPublisher;
         this.activeOrderRepo = activeOrderRepo;
+        this.authenticationService = authenticationService;
     }
 
     @Override
@@ -75,69 +77,71 @@ public class ActiveOrderService implements IActiveOrderService {
         return true; // Placeholder return value
     }
 
-    public boolean startOrder(SessionToken sessionToken, String eventID)
-    {
-//        if(AuthenticationService.validateToken(sessionToken.getToken())){
+//    public boolean startOrder(SessionToken sessionToken, String eventID) {
+//        if(authenticationService.validateToken(sessionToken.getToken())){
+//            try {
+//                //check if user is member, if he is then need to save his order
+//                boolean isMember = activeOrderPublisher.publishIsMember(sessionToken.getUserId());
 //
+//                // get Seating map of event and show it
+//                // SeatingMapDTO map = activeOrderPublisher.publishGetSeatingMapEvent(eventID);
+//                // user choose the tickets
+//                /**
+//                 *  need to make sure the same user cant reserve different seats/tickets from different session
+//                 */
+//                /**
+//                 * need to start a thread that checks always if the timer has ended
+//                 */
+//                /**
+//                 * need to publish a message to unreserve tickets if guest disconnects or 10 minutes pass for memeber
+//                 */
+//                String orderId = "" + IdGenerator.getInstance().nextId();
+//                ActiveOrderItem orderItem = new ActiveOrderItem(orderId, sessionToken.getUserId(), eventID, 0);
+//                Scanner scanner = new Scanner(System.in);
+//                boolean finished = false;
+//                while (!finished) {
+//                    System.out.println("choose the number of tickets to buy, or choose 'finish' ");
+//                    String input = scanner.nextLine();
+//                    if (input.toLowerCase().equals("finish")) {
+//                        break;
+//                    }
+//                    int inputInt;
+//                    try {
+//                        inputInt = Integer.parseInt(input);
+//                        if (inputInt <= 0) {
+//                            System.out.println("please enter a positive number of tickets");
+//                        }
+//                    } catch (NumberFormatException e) {
+//                        System.out.println("please enter either 'finish' or a number of tickets to buy");
+//                        continue;
+//                    }
+//                    boolean reservedTickets = activeOrderPublisher.publishReserveTickets(eventID, inputInt);
+//                    if (!reservedTickets) {
+//                        System.out.println("there are not enough tickets left currently ");
+//                    } else {
+//                        orderItem.setQuantity(inputInt);
+//                        if (isMember) {
+//                            saveOrder(orderItem);
+//                        }
+//                    }
+//                }
+//                System.out.println("you have " + orderItem.getQuantity() + " tickets in your order, would you like to checkout? (y/n)");
+//                String choice = scanner.nextLine();
+//                if (choice.toLowerCase().equals("y") || choice.toLowerCase().equals("yes")) {
+//                    completeActiveOrder(orderId);
+//                } else {
+//                    System.out.println("order canceled");
+//                    activeOrderPublisher.publishUnreserveTickets(eventID, orderItem.getQuantity());
+//                }
+//                return true;
+//            }catch (Exception e){
+//                System.out.println("got an error while trying to start order: "+ e.getMessage());
+//                return false;
+//            }
 //        }
 //        else{
+//            System.out.println("the session has ended");
 //            return false;
 //        }
-        //check if user is member, if he is then need to save his order
-        boolean isMember = activeOrderPublisher.publishIsMember(sessionToken.getUserId());
-
-        // get Seating map of event and show it
-        // SeatingMapDTO map = activeOrderPublisher.publishGetSeatingMapEvent(eventID);
-        // user choose the tickets
-        /**
-         *  need to make sure the same user cant reserve different seats/tickets from different session
-         */
-        /**
-         * need to start a thread that checks always if the timer has ended
-         */
-        /**
-         * need to publish a message to unreserve tickets if guest disconnects or 10 minutes pass for memeber
-         */
-        String orderId = "" +IdGenerator.getInstance().nextId();
-
-        ActiveOrderItem orderItem = new ActiveOrderItem(orderId,sessionToken.getUserId(),eventID,0 );
-        Scanner scanner = new Scanner(System.in);
-        boolean finished = false;
-        while(!finished) {
-            System.out.println("choose the number of tickets to buy, or choose 'finish' ");
-            String input = scanner.nextLine();
-            if(input.toLowerCase().equals("finish")){
-                break;
-            }
-            int inputInt;
-            try {
-                inputInt = Integer.parseInt(input);
-                if(inputInt <= 0){
-                    System.out.println("please enter a positive number of tickets");
-                }
-            }catch (NumberFormatException e){
-                System.out.println("please enter either 'finish' or a number of tickets to buy");
-                continue;
-            }
-            boolean reservedTickets = activeOrderPublisher.publishReserveTickets(eventID, inputInt);
-            if (!reservedTickets) {
-                System.out.println("there are not enough tickets left currently ");
-            }
-            else {
-                orderItem.setQuantity(inputInt);
-                if (isMember) {
-                    saveOrder(orderItem);
-                }
-            }
-        }
-        System.out.println("you have " + orderItem.getQuantity() + " tickets in your order, would you like to checkout? (y/n)");
-        String choice = scanner.nextLine();
-        if(choice.toLowerCase().equals("y") || choice.toLowerCase().equals("yes")){
-            completeActiveOrder(orderId);
-        }
-        else{
-            System.out.println("order canceled");
-            activeOrderPublisher.publishUnreserveTickets(eventID, orderItem.getQuantity());
-        }
-    }
+//    }
 }
