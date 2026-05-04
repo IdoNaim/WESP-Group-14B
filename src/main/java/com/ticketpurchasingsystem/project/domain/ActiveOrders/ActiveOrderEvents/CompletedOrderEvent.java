bb38359f87fa4b51ae5885bbdc642e4bca671084
IdoNaim <idonai@post.bgu.ac.il>
2026-05-04 12:46:52 +0300

    implemented CompletedOrderEvent

--- src/main/java/com/ticketpurchasingsystem/project/domain/ActiveOrders/ActiveOrderEvents/CompletedOrderEvent.java
+++ src/main/java/com/ticketpurchasingsystem/project/domain/ActiveOrders/ActiveOrderEvents/CompletedOrderEvent.java
@@ -1,4 +1,22 @@
 package com.ticketpurchasingsystem.project.domain.ActiveOrders.ActiveOrderEvents;
 
-public class CompletedOrderEvent {
+import com.ticketpurchasingsystem.project.domain.ActiveOrders.ActiveOrderDTO;
+import org.springframework.context.ApplicationEvent;
+
+public class CompletedOrderEvent extends ApplicationEvent {
+    private ActiveOrderDTO order;
+    private double amountPaid;
+    public CompletedOrderEvent(Object source, ActiveOrderDTO order, double amountPaid){
+        super(source);
+        this.order = order;
+        this.amountPaid = amountPaid;
+    }
+
+    public ActiveOrderDTO getOrder() {
+        return order;
+    }
+
+    public double getAmountPaid() {
+        return amountPaid;
+    }
 }
