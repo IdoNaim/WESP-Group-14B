aa408e2482fba7c5bee223273e96c34c3428101d
IdoNaim <idonai@post.bgu.ac.il>
2026-05-04 12:42:52 +0300

    implemented IsUpToPolicyEvent

--- src/main/java/com/ticketpurchasingsystem/project/domain/ActiveOrders/ActiveOrderEvents/IsUpToPolicyEvent.java
+++ src/main/java/com/ticketpurchasingsystem/project/domain/ActiveOrders/ActiveOrderEvents/IsUpToPolicyEvent.java
@@ -1,4 +1,32 @@
 package com.ticketpurchasingsystem.project.domain.ActiveOrders.ActiveOrderEvents;
 
-public class IsUpToPolicyEvent {
+import com.ticketpurchasingsystem.project.domain.ActiveOrders.ActiveOrderDTO;
+import org.springframework.context.ApplicationEvent;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class IsUpToPolicyEvent extends ApplicationEvent {
+    private ActiveOrderDTO order;
+    public IsUpToPolicyEvent(Object source, ActiveOrderDTO order){
+        super(source);
+        this.order = order;
+    }
+
+    public ActiveOrderDTO getOrder() {
+        return order;
+    }
+    public String getEventID(){
+        return order.getEventId();
+    }
+    public String userID(){
+        return order.getUserId();
+    }
+    public List<String> getSeatIds(){
+        return order.getSeatIds();
+    }
+    public HashMap<String, Integer>  getStandingAreaQuantities(){
+        return order.getStandingAreaQuantities();
+    }
+
 }
