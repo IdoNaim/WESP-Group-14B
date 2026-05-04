eab6445131792687a066e00b2bcb24c53b1b5000
IdoNaim <idonai@post.bgu.ac.il>
2026-05-04 15:10:49 +0300

    changed field seatIds in SeatReservationEvent to List<String>

--- src/main/java/com/ticketpurchasingsystem/project/domain/ActiveOrders/ActiveOrderEvents/SeatReservationEvent.java
+++ src/main/java/com/ticketpurchasingsystem/project/domain/ActiveOrders/ActiveOrderEvents/SeatReservationEvent.java
@@ -2,19 +2,21 @@ package com.ticketpurchasingsystem.project.domain.ActiveOrders.ActiveOrderEvents
 
 import org.springframework.context.ApplicationEvent;
 
+import java.util.List;
+
 public class SeatReservationEvent extends ApplicationEvent{
-    String[] seatIds;
+    List<String> seatIds;
     String eventID;
     Boolean result;
 
-    public SeatReservationEvent(Object source, String eventID ,String[] seatIds) {
+    public SeatReservationEvent(Object source, String eventID , List<String> seatIds) {
         super(source);
         this.seatIds = seatIds;
         this.eventID = eventID;
         result = null;
     }
 
-    public String[] getSeatIds() {
+    public List<String> getSeatIds() {
         return seatIds;
     }
 
