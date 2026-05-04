3ecd1058ce955835ea342901951f5696605d89b9
IdoNaim <idonai@post.bgu.ac.il>
2026-05-04 12:38:14 +0300

    implemented SeatReleaseEvent and StandingAreaReleaseEvent

--- src/main/java/com/ticketpurchasingsystem/project/domain/ActiveOrders/ActiveOrderEvents/SeatReleaseEvent.java
+++ src/main/java/com/ticketpurchasingsystem/project/domain/ActiveOrders/ActiveOrderEvents/SeatReleaseEvent.java
@@ -1,4 +1,21 @@
 package com.ticketpurchasingsystem.project.domain.ActiveOrders.ActiveOrderEvents;
 
-public class SeatReleaseEvent {
+import org.springframework.context.ApplicationEvent;
+
+public class SeatReleaseEvent extends ApplicationEvent {
+    private String eventID;
+    private String[] seatsIds;
+    public SeatReleaseEvent(Object source, String eventID, String[] seatIds){
+        super(source);
+        this.eventID= eventID;
+        this.seatsIds = seatIds;
+    }
+
+    public String getEventID() {
+        return eventID;
+    }
+
+    public String[] getSeatsIds() {
+        return seatsIds;
+    }
 }
