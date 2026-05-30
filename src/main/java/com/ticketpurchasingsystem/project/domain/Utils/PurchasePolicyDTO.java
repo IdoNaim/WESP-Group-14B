babb26da1b402f6ac0cabfc9bd60fb7b44f6a161
edenyaakobi <edens27@gmail.com>
2026-05-30 18:17:51 +0300

    fixed ci

--- src/main/java/com/ticketpurchasingsystem/project/domain/Utils/PurchasePolicyDTO.java
+++ src/main/java/com/ticketpurchasingsystem/project/domain/Utils/PurchasePolicyDTO.java
@@ -1,9 +1,15 @@
 package com.ticketpurchasingsystem.project.domain.Utils;
 public record PurchasePolicyDTO(
-    Integer minTickets,
-    Integer maxTickets,
-    Integer minAge,
-    Integer maxAge,
-    boolean emnptySeatLeft
-    //add routes 
+        // Quantity Rules Block
+        Integer minTickets,
+        Integer maxTickets,
+        boolean isQuantityOr,        // false = AND, true = OR (e.g., max 2 OR min 100)
+
+        // Age Rules Block
+        Integer minAge,
+        Integer maxAge,
+        boolean isAgeOr,             // false = AND, true = OR
+
+        // Outer Composition Block
+        boolean isAgeAndQuantityOr  // false = AND, true = OR (Combines Age block with Quantity block)
 ) {}
