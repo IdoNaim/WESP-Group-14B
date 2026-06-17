f15a4e77f08b9d2653dc8fb45c93882bcba65453
IdoNaim <idonai@post.bgu.ac.il>
2026-06-18 00:58:41 +0300

    deleted InitException, dont need it

--- src/main/java/com/ticketpurchasingsystem/project/init/InitException.java
+++
@@ -1,7 +0,0 @@
-package com.ticketpurchasingsystem.project.init;
-
-public class InitException extends RuntimeException{
-    public InitException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
