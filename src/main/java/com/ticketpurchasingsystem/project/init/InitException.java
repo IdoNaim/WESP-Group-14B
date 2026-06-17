12594d6bc23b94e7de9239d9426621e35b95d2a5
IdoNaim <idonai@post.bgu.ac.il>
2026-06-18 00:49:39 +0300

    added InitException

---
+++ src/main/java/com/ticketpurchasingsystem/project/init/InitException.java
@@ -0,0 +1,7 @@
+package com.ticketpurchasingsystem.project.init;
+
+public class InitException extends RuntimeException{
+    public InitException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
